c3eb2770422be15deb71e70e00a296210b734ec1
hoodielive <hoodielive@gmail.com>
2019-12-23 10:28:19 -0500

    reOrg

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
 
 app.use(express.json({ extended: false }))
+
+// Connect to Database.
+connectDB(); 
 
 const PORT = 5000 
 
